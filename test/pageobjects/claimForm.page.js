f061b816cc1f47d957be205623a5745b1f614580
Ariane Gomes <ariane@estudante.ufscar.br>
2024-08-09 19:30:34 -0300

    Initial commit

---
+++ test/pageobjects/claimForm.page.js
@@ -0,0 +1,40 @@
+class claimForm {
+
+    get name() { return $("#c__ClaimantInfo_Name_ee85a0da") }
+    get email() { return $("#c__ClaimantInfo_Email_ee85a0da") }
+    get addPurchase() { return $("//button[normalize-space()='Add A Purchase']") }
+    get date() { return $("//input[@type='date']") }
+    get stateDropdown() { return $(".null-value")}
+    get confirmation() { return $("//span[@tabindex='0']") }
+    get signature() { return $("#c__Signature_SignatureText_ee85a0da") }
+    get submitButton() { return $("//button[normalize-space()='Submit']") }
+
+    open() {
+        return browser.url('https://cms.template4.jndla.net/claim-form')
+    }
+
+    async submit(name, email, date, state, signature) {
+        await browser.maximizeWindow()
+
+        await this.name.setValue(name)
+        await this.email.setValue(email)
+
+        await this.addPurchase.click()
+        await this.date.click()
+        await this.date.setValue(date)
+
+        await this.stateDropdown.click()
+        await $(`//li[normalize-space()='${state}']`).click()
+
+        await this.confirmation.click()
+        await this.signature.setValue(signature)
+
+        await browser.pause(2000);
+
+        await this.submitButton.click()
+
+        await browser.pause(500);
+    }
+}
+
+export default new claimForm();
---
+++ test/specs/claimForm.spec.js
@@ -0,0 +1,23 @@
+import claimForm from '../pageobjects/claimForm.page.js';
+
+describe('Claim Form Automation Test', async () => {
+
+    it('Success Case', async () => {
+        await claimForm.open()
+
+        await claimForm.submit('Ariane', 'test@thinklogic.com', '08/09/2024', 'CA', 'Ariane Gomes')
+        
+        const element = await $('.container').getText();
+        await expect(element).toEqual(expect.stringContaining('Your claim has been submitted.'));
+    });
+
+    it('Failed Case - Missing Info', async () => {
+        await claimForm.open()
+
+        await claimForm.submit('Ariane', '', '08/09/2024', 'CA', '')
+
+        const element = await $('.container').getText();
+        await expect(element).toEqual(expect.stringContaining('*required'));
+    })
+
+});
